6f14099424b70f81c5745ddb9f1200547b870402
Murtadha Marzouq <mmarzou0@gmail.com>
2022-10-27 22:32:30 -0400

    😜

--- OOP in Powershell/Animal_Cat_OOP/Cat.ps1
+++ OOP in Powershell/Animal_Cat_OOP/Cat.ps1
@@ -7,11 +7,7 @@ import-module ./Animal.ps1
 class Cat : Animal {
     [string]$color
     [string]$breed
-
-    Cat([string]$name, [string]$habitat, [string]$sound, [int]$population, [string]$color, [string]$breed) : base($name, $habitat, $sound, $population){
-        $this.color = $color
-        $this.breed = $breed
-    }
+    
 
     [void]setColor([string]$color){
         $this.color = $color
